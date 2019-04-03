214ca0939cf9f33d7b3cde70ed36bd26a76574c9
Taras <taraskozub19@gmail.com>
2019-04-03 22:54:24 +0300

    develop track list screen.

--- music-player-app/app/components/TrackListScreen.js
+++ music-player-app/app/components/TrackListScreen.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, TouchableHighlight } from "react-native";
 import { Actions } from "react-native-router-flux";
 
-const TrackList = () => {
+const TrackList = ({ tracks }) => {
+  const atLeastOneTrack = tracks[0];
   return (
     <View style={styles.container}>
-      <Text style={styles.header} onPress={() => Actions.AudioPlayer()}>trackList</Text>
+      <Text style={styles.header} onPress={() => Actions.AudioPlayer()}>
+        Track list
+      </Text>
+      {atLeastOneTrack ? (
+        <FlatList
+          style={styles.trackList}
+          data={tracks}
+          renderItem={track => (
+            <TouchableHighlight style={styles.trackListItem}>
+              <Text style={styles.trackListItemText}>{track.item.trackName}</Text>
+            </TouchableHighlight>
+          )}
+        />
+      ) : (
+        <Text>loading...</Text>
+      )}
     </View>
   );
 };
@@ -15,13 +31,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    backgroundColor: "blue",
+    backgroundColor: "white",
     height: "100%",
-    padding: 40
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 30
   },
   header: {
     fontSize: 24,
-    color: "white"
+    color: "black"
+  },
+  trackList: {
+    width: "100%"
+  },
+  trackListItem: { paddingBottom: 20, paddingTop: 20, width: "100%", borderBottomWidth: 1, borderBottomColor: "black" },
+  trackListItemText: {
+    color: "black", 
+    fontSize: 16
   }
 });
 
